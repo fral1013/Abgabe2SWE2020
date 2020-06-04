61f0f78fbceb98111436ba3f3a53ba58e070ea3e
fral1013 <56153613+fral1013@users.noreply.github.com>
2020-06-04 11:16:08 +0200

    routing

--- src/app/buch/kunde.module.ts
+++ src/app/buch/kunde.module.ts
@@ -16,9 +16,9 @@
  */
 
 import { BalkendiagrammModule } from './diagramme/balkendiagramm.module';
-import { KundeRoutingModule } from './kunde-routing.module';
 import { CreateKundeModule } from './create-kunde/create-kunde.module';
 import { DetailsKundeModule } from './details-kunde/details-kunde.module';
+import { KundeRoutingModule } from './kunde-routing.module';
 import { LiniendiagrammModule } from './diagramme/liniendiagramm.module';
 import { NgModule } from '@angular/core';
 import { SucheKundenModule } from './suche-kunden/suche-kunden.module';
