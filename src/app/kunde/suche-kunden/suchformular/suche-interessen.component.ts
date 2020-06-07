cf7fb0d49a8fb2ef0853abea07b2b0d07b19b1a4
sschach <63590284+sschach@users.noreply.github.com>
2020-06-07 14:44:10 +0200

    suche interessen

---
+++ src/app/kunde/suche-kunden/suchformular/suche-interessen.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/**
+ * Komponente f&uuml;r das Tag <code>hs-suche-schlagwoerter</code>
+ */
+@Component({
+    selector: 'hs-suche-interessen',
+    templateUrl: './suche-interessen.component.html',
+})
+export class SucheInteressenComponent {
+    lesen = false;
+
+    reisen = false;
+
+    sport = false;
+    constructor() {
+        console.log('SucheInteressenComponent.constructor()');
+    }
+}
