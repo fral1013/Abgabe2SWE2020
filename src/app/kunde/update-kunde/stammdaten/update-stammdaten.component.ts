e45babc09d0141b02f9321b7b5a5ec59e8ec4cb3
sschach <63590284+sschach@users.noreply.github.com>
2020-06-07 16:59:23 +0200

    update stammdaten

--- src/app/kunde/update-kunde/stammdaten/update-stammdaten.component.ts
+++ src/app/kunde/update-kunde/stammdaten/update-stammdaten.component.ts
@@ -1,21 +1,4 @@
-/*
- * Copyright (C) 2015 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import { Buch, BuchService } from '../../shared';
+import { Kunde, KundeService } from '../../shared';
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { HOME_PATH } from '../../../shared';
@@ -30,14 +13,13 @@ import { Router } from '@angular/router';
     templateUrl: './update-stammdaten.component.html',
 })
 export class UpdateStammdatenComponent implements OnInit {
-    // <hs-update-stammdaten [buch]="...">
     @Input()
-    readonly buch!: Buch;
+    readonly kunde!: Kunde;
 
     readonly form = new FormGroup({});
 
     constructor(
-        private readonly buchService: BuchService,
+        private readonly kundeService: KundeService,
         private readonly router: Router,
     ) {
         console.log('UpdateStammdatenComponent.constructor()');
@@ -45,14 +27,14 @@ export class UpdateStammdatenComponent implements OnInit {
 
     /**
      * Das Formular als Gruppe von Controls initialisieren und mit den
-     * Stammdaten des zu &auml;ndernden Buchs vorbelegen.
+     * Stammdaten des zu &auml;ndernden Kundes vorbelegen.
      */
     ngOnInit() {
-        console.log('UpdateStammdatenComponent.ngOnInit(): buch=', this.buch);
+        console.log('UpdateStammdatenComponent.ngOnInit(): kunde=', this.kunde);
     }
 
     /**
-     * Die aktuellen Stammdaten f&uuml;r das angezeigte Buch-Objekt
+     * Die aktuellen Stammdaten f&uuml;r das angezeigte Kunde-Objekt
      * zur&uuml;ckschreiben.
      * @return false, um das durch den Button-Klick ausgel&ouml;ste Ereignis
      *         zu konsumieren.
@@ -65,25 +47,22 @@ export class UpdateStammdatenComponent implements OnInit {
             return;
         }
 
-        if (this.buch === undefined) {
+        if (this.kunde === undefined) {
             console.error(
-                'UpdateStammdatenComponent.onUpdate(): buch === undefined',
+                'UpdateStammdatenComponent.onUpdate(): kunde === undefined',
             );
             return;
         }
 
         // rating, preis und rabatt koennen im Formular nicht geaendert werden
-        this.buch.updateStammdaten(
-            this.form.value.titel,
-            this.form.value.art,
-            this.form.value.verlag,
-            this.form.value.rating,
-            this.buch.datum,
-            this.buch.preis,
-            this.buch.rabatt,
-            this.form.value.isbn,
+        this.kunde.updateStammdaten(
+            this.form.value.nachname,
+            this.form.value.geschlecht,
+            this.form.value.familienstand,
+            this.form.value.kategorie,
+            this.kunde.geburtsdatum,
         );
-        console.log('buch=', this.buch);
+        console.log('kunde=', this.kunde);
 
         const successFn = async () => {
             console.log(
@@ -103,11 +82,8 @@ export class UpdateStammdatenComponent implements OnInit {
             // TODO Fehlermeldung anzeigen
         };
 
-        await this.buchService.update(this.buch, successFn, errorFn);
+        await this.kundeService.update(this.kunde, successFn, errorFn);
 
-        // damit das (Submit-) Ereignis konsumiert wird und nicht an
-        // uebergeordnete Eltern-Komponenten propagiert wird bis zum
-        // Refresh der gesamten Seite
         return false;
     }
 }
--- src/app/kunde/update-kunde/stammdaten/update-stammdaten.module.ts
+++ src/app/kunde/update-kunde/stammdaten/update-stammdaten.module.ts
@@ -1,25 +1,8 @@
-/*
- * Copyright (C) 2019 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { UpdateArtModule } from './update-art.module';
+import { UpdateGeschlechtModule } from './update-geschlecht.module';
 import { UpdateNachnameModule } from './update-nachname.module';
-import { UpdateRatingModule } from './update-rating.module';
+import { UpdateKategorieModule } from './update-kategorie.module';
 import { UpdateStammdatenComponent } from './update-stammdaten.component';
 import { UpdateVerlagModule } from './update-verlag.module';
 
@@ -28,10 +11,10 @@ import { UpdateVerlagModule } from './update-verlag.module';
     exports: [UpdateStammdatenComponent],
     imports: [
         ReactiveFormsModule,
-        UpdateArtModule,
-        UpdateRatingModule,
+        UpdateGeschlechtModule,
+        UpdateKategorieModule,
         UpdateNachnameModule,
-        UpdateVerlagModule,
+        UpdateFamilienstandModule,
     ],
 })
 export class UpdateStammdatenModule {}
