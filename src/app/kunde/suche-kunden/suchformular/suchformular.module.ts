2eb098ac6c4d8591a7393dc1bbeb9c8c6272af4e
fral1013 <56153613+fral1013@users.noreply.github.com>
2020-06-15 11:30:50 +0200

    Revert "Kleinere Fixes"
    This reverts commit 728a4af6bc303033e1bc638b3b3db1f142d9170e.

--- src/app/kunde/suche-kunden/suchformular/suchformular.module.ts
+++ src/app/kunde/suche-kunden/suchformular/suchformular.module.ts
@@ -2,10 +2,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { SucheFamilienstandModule } from './suche-familienstand.module';
 import { SucheGeschlechtModule } from './suche-geschlecht.module';
 import { SucheInteressenModule } from './suche-interessen.module';
 import { SucheNachnameModule } from './suche-nachname.module';
+import { SucheFamilienstandModule } from './suche-familienstand.module';
 import { SuchformularComponent } from './suchformular.component';
 
 @NgModule({
